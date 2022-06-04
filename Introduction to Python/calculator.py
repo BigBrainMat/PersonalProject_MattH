5be9c7f62cdd89250136065de473c5883835f770
Matt Harrisburg <matthew.harrisburg@gmail.com>
2022-06-03 23:25:34 -0400

    Calculator made in Python
    Using some of the skills learned I put together a calculator

---
+++ Introduction to Python/calculator.py
@@ -0,0 +1,3 @@
+first_number = int(input('Type the first number: ')) #Gets the first number from the user
+second_number = int(input('Type the second number: ')) #Gets the second number from the user
+print("The sum is: ", first_number + second_number) #Gives us the sum of thoes two numbers 
